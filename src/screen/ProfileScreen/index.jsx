34e43d3d6ac83cbeccc6b9a843315c650cd1f7ee
rereard <rereardany60@gmail.com>
2022-11-16 15:27:00 +0700

    add screens and tab navigator

---
+++ src/screen/ProfileScreen/index.jsx
@@ -0,0 +1,17 @@
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+    FlatList
+  } from 'react-native';
+export default function ProfileScreen(){
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Profile</Text>
+        </View>
+    );
+}
---
+++ src/screen/SearchScreen/index.jsx
@@ -0,0 +1,17 @@
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+    FlatList
+  } from 'react-native';
+export default function SearchScreen(){
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+            <Text>Home and Search</Text>
+        </View>
+    );
+}
