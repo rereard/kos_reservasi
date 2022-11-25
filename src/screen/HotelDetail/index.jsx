6415063b980f36155c0f36028fc8ab424d4a71c0
muchlisaryn <muchlisar68@gmail.com>
2022-11-26 01:46:36 +0700

    create description at detail hotel

--- src/screen/HotelDetail/index.jsx
+++ src/screen/HotelDetail/index.jsx
@@ -6,7 +6,7 @@ import {
   SafeAreaView,
   ScrollView,
 } from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import Header from '../../component/molecules/Header';
 import {colors} from '../../utils';
 import RatingStar from '../../component/atoms/RatingStar';
@@ -14,6 +14,16 @@ import Button from '../../component/atoms/Button';
 import Review from '../../component/molecules/Review';
 
 export default function DetailHotel({navigation}) {
+  const [lineText, setLineText] = useState(3);
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const readMore = () => {
+    if (lineText === 0) {
+      setLineText(3);
+    } else {
+      setLineText(0);
+    }
+  };
   return (
     <SafeAreaView>
       <ScrollView>
@@ -33,22 +43,40 @@ export default function DetailHotel({navigation}) {
           </View>
         </View>
         <View style={styles.container}>
-          <Text style={styles.title}>Hotel Name</Text>
-          <View style={{flexDirection: 'row'}}>
-            <RatingStar />
-            <View
-              style={{
-                marginLeft: 10,
-                flexDirection: 'row',
-                alignItems: 'center',
-              }}>
+          <View
+            style={{
+              justifyContent: 'space-between',
+              flexDirection: 'row',
+              alignItems: 'center',
+            }}>
+            <View>
+              <Text style={styles.title}>Hotel Name</Text>
+              <View style={{flexDirection: 'row'}}>
+                <RatingStar />
+                <View
+                  style={{
+                    marginLeft: 10,
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                  }}>
+                  <Button
+                    type="icon"
+                    icon={'location-outline'}
+                    color={colors.darkBlue}
+                    size={15}
+                  />
+                  <Text style={{marginLeft: 5}}>Jakarta</Text>
+                </View>
+              </View>
+            </View>
+            <View>
               <Button
                 type="icon"
-                icon={'location-outline'}
-                color={colors.darkBlue}
-                size={15}
+                icon={isFavorite ? 'heart' : 'heart-outline'}
+                onPress={() => setIsFavorite(!isFavorite)}
+                color={isFavorite ? colors.pink : colors.darkBlue}
+                size={30}
               />
-              <Text style={{marginLeft: 5}}>Jakarta</Text>
             </View>
           </View>
           <ScrollView
@@ -62,6 +90,27 @@ export default function DetailHotel({navigation}) {
             <Text style={styles.facilities}>Airport transfers</Text>
             <Text style={styles.facilities}>24-hour front desk</Text>
           </ScrollView>
+          <View>
+            <Text numberOfLines={lineText} style={styles.description}>
+              Located in Jakarta, within 5.7 km of Pondok Indah Mall and 6 km of
+              Pacific Place, RedDoorz Plus near Lippo Mall Kemang 2 offers
+              accommodation with a garden. The property is around 6.5 km from
+              Plaza Senayan, 8.4 km from Ragunan Zoo and 10 km from Selamat
+              Datang Monument. The accommodation provides a 24-hour front desk
+              and free WiFi throughout the property. At the guest house each
+              room includes a private bathroom. Grand Indonesia is 10 km from
+              RedDoorz Plus near Lippo Mall Kemang 2, while Sarinah is 11 km
+              away. The nearest airport is Halim Perdanakusuma International
+              Airport, 11 km from the accommodation.
+            </Text>
+            {lineText === 0 ? (
+              <></>
+            ) : (
+              <Text onPress={readMore} style={{color: colors.darkBlue}}>
+                Read More..
+              </Text>
+            )}
+          </View>
           <View>
             <Text>All Reviews</Text>
             <Review />
@@ -110,5 +159,9 @@ const styles = StyleSheet.create({
     padding: 5,
     borderRadius: 5,
     marginRight: 5,
+  },
+  description: {
+    fontSize: 14,
+    textAlign: 'justify',
   },
 });
