b84631b56603a4021fce72fdba5c57d5a76bd4b7
rereard <rereardany60@gmail.com>
2022-12-01 21:24:17 +0700

    wip hotel image carousel

--- src/screen/HotelDetail/index.jsx
+++ src/screen/HotelDetail/index.jsx
@@ -5,8 +5,10 @@ import {
   StyleSheet,
   SafeAreaView,
   ScrollView,
+  Dimensions,
+  FlatList
 } from 'react-native';
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useRef, useCallback} from 'react';
 import Header from '../../component/molecules/Header';
 import {colors} from '../../utils';
 import Location from './parts/Location';
@@ -18,13 +20,83 @@ import {fetchDetail} from '../../features/detailHotelSlice';
 import {fetchReview} from '../../features/ReviewSlice';
 import Kebijakan from './parts/Kebijakan';
 
+
+
 export default function DetailHotel({route, navigation}) {
   const {hotel_id, checkIn, checkOut, guests, rooms} = route.params;
+  const [hotelPhotos, setHotelPhotos] = useState([])
   const [lineText, setLineText] = useState(3);
   const [isFavorite, setIsFavorite] = useState(false);
   const dispatch = useDispatch();
   const detail = useSelector(state => state.detail.detail);
+  const isPending = useSelector(state => state.detail.isPending);
   const review = useSelector(state => state.review.review);
+
+  useEffect(() => {
+    const fetchData = async() => {
+      try {
+        const response = await axios.request({
+          method: 'GET',
+          url: 'https://apidojo-booking-v1.p.rapidapi.com/properties/get-hotel-photos',
+          params: {hotel_ids: hotel_id, languagecode: 'id'},
+          headers: {
+            'X-RapidAPI-Key': '8acc31ed09mshcd579e2a1d4d065p1adbebjsn5a767b7f288e',
+            'X-RapidAPI-Host': 'apidojo-booking-v1.p.rapidapi.com'
+          }
+        })
+        setHotelPhotos(response.data.data[hotel_id])
+      } catch (e) {
+        throw(e)
+      }
+    }
+    if(detail?.hotel_id !== hotel_id){
+      fetchData()
+    }
+  }, []);
+
+  useEffect(() => {
+    console.log("hotel PHOTOS", hotelPhotos);
+  }, [hotelPhotos]);
+
+  const [index, setIndex] = useState(0);
+  const indexRef = useRef(index);
+  indexRef.current = index;
+  const onScroll = useCallback((event) => {
+    const slideSize = event.nativeEvent.layoutMeasurement.width;
+    const index = event.nativeEvent.contentOffset.x / slideSize;
+    const roundIndex = Math.round(index);
+
+    const distance = Math.abs(roundIndex - index);
+
+    // Prevent one pixel triggering setIndex in the middle
+    // of the transition. With this we have to scroll a bit
+    // more to trigger the index change.
+    const isNoMansLand = 0.4 < distance;
+
+    if (roundIndex !== indexRef.current && !isNoMansLand) {
+      setIndex(roundIndex);
+    }
+  }, []);
+
+  const width = Dimensions.get('window').width;
+  console.log("width", width);
+
+  const flatListOptimizationProps = {
+    initialNumToRender: 0,
+    maxToRenderPerBatch: 1,
+    removeClippedSubviews: true,
+    scrollEventThrottle: 16,
+    keyExtractor: useCallback(e => e.id, []),
+    getItemLayout: useCallback(
+      (_, index) => ({
+        index,
+        length: width,
+        offset: index * width,
+      }),
+      []
+    ),
+  };
+  
 
   // const [description, setDescription] = useState([]);
 
@@ -37,11 +109,15 @@ export default function DetailHotel({route, navigation}) {
   };
 
   useEffect(() => {
-    dispatch(fetchReview(route.params));
+    if(detail?.hotel_id !== hotel_id){
+      dispatch(fetchReview(route.params));
+    }
   }, []);
 
   useEffect(() => {
-    dispatch(fetchDetail(route.params));
+    if(detail?.hotel_id !== hotel_id){
+      dispatch(fetchDetail(route.params));
+    }
   }, []);
 
   // console.log('==>', description);
@@ -71,112 +147,146 @@ export default function DetailHotel({route, navigation}) {
 
   return (
     <SafeAreaView style={{backgroundColor: colors.white, flex: 1}}>
-      <ScrollView>
-        <View>
-          <Image
-            source={{
-              uri: 'https://img.inews.co.id/media/822/files/inews_new/2022/03/25/Hotel_Dekat_Malioboro.jpg',
-            }}
-            style={styles.image}
-          />
-          <View style={styles.header}>
-            <Header onPress={() => navigation.goBack()} />
-          </View>
-          <View style={styles.boxPrice}>
-            <Text style={styles.price}>RP 200.000</Text>
-            <Text style={styles.price}>/Night</Text>
-          </View>
+      {isPending ? (
+        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+          <Text style={styles.title}>Loading...</Text>
         </View>
-        <View style={styles.container}>
-          <View
-            style={{
-              justifyContent: 'space-between',
-              flexDirection: 'row',
-              alignItems: 'center',
-            }}>
+      ) : (
+        <>
+          <ScrollView>
             <View>
-              <Text style={styles.title}>{detail.hotel_name}</Text>
-              <View
-                style={{
-                  flexDirection: 'row',
-                  alignItems: 'center',
-                  marginTop: 5,
-                }}>
-                <Button
-                  type="icon"
-                  icon={'location-outline'}
-                  color={colors.darkBlue}
-                  size={15}
-                />
-                <Text style={{marginLeft: 5, color: colors.darkGrey}}>{detail?.city}</Text>
+              {/* <ScrollView 
+                horizontal={true}
+                style={{flexDirection: 'row', borderWidth: 2}}
+              >
+                {hotelPhotos.map((item, index) => {
+                  <Image
+                    key={index}
+                    source={{
+                      uri: `https://cf.bstatic.com${item[6]}`,
+                    }}
+                    style={styles.image}
+                  />
+                })}
+              </ScrollView> */}
+              <FlatList
+                data={hotelPhotos}
+                style={styles.image}
+                renderItem={({ item }) => (
+                  <Image
+                    source={{
+                      uri: `https://cf.bstatic.com${item[6]}`,
+                    }}
+                    style={styles.image}
+                  />
+                )}
+                pagingEnabled
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                onScroll={onScroll}
+                {...flatListOptimizationProps}
+              />
+              <View style={styles.header}>
+                <Header onPress={() => navigation.goBack()} />
+              </View>
+              <View style={styles.boxPrice}>
+                <Text style={styles.price}>RP 200.000</Text>
+                <Text style={styles.price}>/Night</Text>
               </View>
             </View>
-          </View>
-          <ScrollView
-            style={{flexDirection: 'row', marginVertical: 10}}
-            horizontal={true}>
-            {detail?.facilities_block?.facilities?.map((item, index) => (
-              <Text key={index} style={styles.facilities}>{item.name}</Text>
-            ))}
-          </ScrollView>
-          {/* {description ? (
-            <View>
-              <Text numberOfLines={lineText} style={styles.description}>
-                {description[1]
-                  ? description[1]?.description
-                  : description[0]?.description}
-              </Text>
-              {lineText === 0 ? (
-                <></>
+            <View style={styles.container}>
+              <View
+                style={{
+                  justifyContent: 'space-between',
+                  flexDirection: 'row',
+                  alignItems: 'center',
+                }}>
+                <View>
+                  <Text style={styles.title}>{detail.hotel_name}</Text>
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      alignItems: 'center',
+                      marginTop: 5,
+                    }}>
+                    <Button
+                      type="icon"
+                      icon={'location-outline'}
+                      color={colors.darkBlue}
+                      size={15}
+                    />
+                    <Text style={{marginLeft: 5, color: colors.darkGrey}}>{detail?.city}</Text>
+                  </View>
+                </View>
+              </View>
+              <ScrollView
+                style={{flexDirection: 'row', marginVertical: 10}}
+                horizontal={true}>
+                {detail?.facilities_block?.facilities?.map((item, index) => (
+                  <Text key={index} style={styles.facilities}>{item.name}</Text>
+                ))}
+              </ScrollView>
+              {/* {description ? (
+                <View>
+                  <Text numberOfLines={lineText} style={styles.description}>
+                    {description[1]
+                      ? description[1]?.description
+                      : description[0]?.description}
+                  </Text>
+                  {lineText === 0 ? (
+                    <></>
+                  ) : (
+                    <Text onPress={readMore} style={{color: colors.darkBlue}}>
+                      Read More..
+                    </Text>
+                  )}
+                </View>
               ) : (
-                <Text onPress={readMore} style={{color: colors.darkBlue}}>
-                  Read More..
-                </Text>
-              )}
+                <></>
+              )} */}
             </View>
-          ) : (
-            <></>
-          )} */}
-        </View>
-        <View style={styles.cardItem}>
-          <ReviewParts
-            data={review}
-            onPress={() => navigation.navigate('Review')}
-          />
-        </View>
-        <View
-          style={{
-            borderBottomWidth: 2,
-            borderBottomColor: colors.grey,
-          }}>
-          <Location address={detail?.address} />
-        </View>
-        <View
-          style={{
-            marginBottom: 90,
-          }}>
-          <Kebijakan data={detail?.hotel_text} />
-        </View>
-      </ScrollView>
-      <View style={styles.SelectRoom}>
-        <View>
-          <Text style={{fontSize: 10, color: colors.white}}>Harga Mulai</Text>
-          <Text
-            style={{color: colors.yellow, fontWeight: 'bold', fontSize: 15}}>
-            200.000
-            <Text
-              style={{color: colors.white, fontWeight: 'normal', fontSize: 10}}>
-              /malam
-            </Text>
-          </Text>
-        </View>
-        <Button
-          title="Pilih Kamar"
-          color={colors.yellow}
-          size={10}
-          width={100}
-        />
-      </View>
+            <View style={styles.cardItem}>
+              <ReviewParts
+                data={review}
+                onPress={() => navigation.navigate('Review')}
+              />
+            </View>
+            <View
+              style={{
+                borderBottomWidth: 2,
+                borderBottomColor: colors.grey,
+              }}>
+              <Location address={detail?.address} />
+            </View>
+            <View
+              style={{
+                marginBottom: 90,
+              }}>
+              <Kebijakan data={detail?.hotel_text} />
+            </View>
+          </ScrollView>
+          <View style={styles.SelectRoom}>
+            <View>
+              <Text style={{fontSize: 10, color: colors.white}}>Harga Mulai</Text>
+              <Text
+                style={{color: colors.yellow, fontWeight: 'bold', fontSize: 15}}>
+                200.000
+                <Text
+                  style={{color: colors.white, fontWeight: 'normal', fontSize: 10}}>
+                  /malam
+                </Text>
+              </Text>
+            </View>
+            <Button
+              title="Pilih Kamar"
+              color={colors.yellow}
+              size={10}
+              width={100}
+            />
+          </View>
+        </>
+      )}
+      
     </SafeAreaView>
   );
 }
