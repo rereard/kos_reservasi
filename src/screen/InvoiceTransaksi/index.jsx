3c6886ec08d825a46fb842406b064f6eeebd4a89
rereard <rereardany60@gmail.com>
2023-06-26 20:08:04 +0700

    revisi

--- src/screen/InvoiceTransaksi/index.jsx
+++ src/screen/InvoiceTransaksi/index.jsx
@@ -11,8 +11,6 @@ import {
   TextInput
 } from 'react-native';
 import { colors } from '../../utils';
-import { useEffect, useState } from 'react';
-import { formatIDR } from '../../utils';
 import { WebView } from 'react-native-webview';
 import { Header } from '../../component/molecules';
 
@@ -24,115 +22,201 @@ export default function InvoiceTransaksi({ navigation, route }) {
       <View style={{ flexDirection: 'row', alignItems: 'center', backgroundColor: colors.darkBlue, padding: 15 }}>
         <Header onPress={() => navigation.goBack()} size={30} title='Invoice Transaksi' />
       </View>
-      <View style={{ height: 5, backgroundColor: colors.white }}></View>
       <WebView
         originWhitelist={['*']}
         source={{
           html: `
-        <html>
-          <head>
-          </head>
-          <body style="padding: 10px 50px">
-            <table border="0" cellpadding="1" cellspacing="1" style="width:100%">
-              <tbody>
-                <tr>
-                  <td style="width:652px">
-                  <h1><span style="font-size:36px"><span style="color:#00a6ff"><strong><span style="font-family:Lucida Sans Unicode,Lucida Grande,sans-serif">Reservasi Kos&nbsp;</span></strong></span></span></h1>
-                  </td>
-                  <td style="text-align:right; width:433px">
-                  <p><span style="font-size:14px">INVOICE</span></p>
-            
-                  <p><span style="font-size:14px">INV/${id_transaksi}</span></p>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          
-            <table border="0" cellpadding="1" cellspacing="1" style="width:100%">
-              <tbody>
-                <tr>
-                  <td style="width:637px">
-                  <p><strong>DITERBITKAN ATAS NAMA</strong></p>
-            
-                  <p>Pemilik Kos: ${nama_pemilik}</p>
-            
-                  <p>&nbsp;</p>
-                  </td>
-                  <td style="width:456px">
-                  <p><strong>UNTUK</strong></p>
-            
-                  <p>Pelanggan: ${nama_pelanggan}</p>
-            
-                  <p>Tanggal Transaksi: ${tanggal_transaksi}</p>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          
-            <p>&nbsp;</p>
-          
-            <table border="0" cellpadding="1" cellspacing="1" style="width:100%">
-              <thead>
-                <tr style="border-width: 20px">
-                  <th scope="col" style="text-align:left; width:33%; border-width: 1px 0px"><span style="font-size:14px"><strong>Nama Kamar</strong></span></th>
-                  <th scope="col" style="text-align:left; width:33%"><span style="font-size:14px"><strong>Nama Kos</strong></span></th>
-                  <th scope="col" style="text-align:left; width:; width:33%"><span style="font-size:14px"><strong>Harga</strong></span></th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr border="0">
-                  <td style="width:33%">
-                  <p style="text-align: left">${nama_kamar}</p>
-            
-                  <p>&nbsp;</p>
-                  </td>
-                  <td style="width:33%">
-                  <p style="text-align: left">${nama_kos}</p>
-            
-                  <p>&nbsp;</p>
-                  </td>
-                  <td style="width:33%">
-                  <p style="text-align: left">Rp ${jumlah_bayar}</p>
-            
-                  <p>&nbsp;</p>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          
-            <p>&nbsp;</p>
-          
-            <table border="0" cellpadding="1" cellspacing="1" style="width:100%">
-              <tbody>
-                <tr>
-                  <td style="text-align:center; width:66%"><strong>TOTAL TAGIHAN</strong></td>
-                  <td style="width:33%"><strong>Rp ${jumlah_bayar}</strong></td>
-                </tr>
-              </tbody>
-            </table>
-            
-            <br />
-            <hr />
-            <br />
-        
-            <table border="0" cellpadding="1" cellspacing="1" style="width:100%">
-              <tbody>
-                <tr>
-                  <td style="text-align:right; width:66%">
-                  <p>&nbsp;</p>
-                  </td>
-                  <td style="width:33%">
-                  <p>Metode Pembayaran:</p>
-            
-                  <p>Bank Transfer</p>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          
-            <p>&nbsp;</p>
-          </body>
-        </html>
+          <html lang="en">
+            <head>
+              <style>
+                .clearfix:after {
+                  content: "";
+                  display: table;
+                  clear: both;
+                }
+              
+                a {
+                  color: #5D6975;
+                  text-decoration: underline;
+                }
+              
+                body {
+                  position: relative;
+                  width: 21cm;  
+                  height: 29.7cm; 
+                  margin: 0 auto; 
+                  color: #001028;
+                  background: #FFFFFF; 
+                  font-family: Arial, sans-serif; 
+                  font-size: 12px; 
+                  font-family: Arial;
+                }
+              
+                header {
+                  padding: 10px 0;
+                  margin-bottom: 30px;
+                }
+                
+                #logo {
+                  text-align: center;
+                  margin-bottom: 10px;
+                }
+                
+                #logo img {
+                  width: 90px;
+                }
+                
+                #project {
+                  float: left;
+                }
+                
+                #project span {
+                  color: #5D6975;
+                  text-align: right;
+                  width: 52px;
+                  margin-right: 10px;
+                  display: inline-block;
+                  font-size: 0.8em;
+                }
+                
+                #company {
+                  float: right;
+                  text-align: right;
+                }
+                
+                #company span {
+                  color: #5D6975;
+                  text-align: right;
+                  width: 52px;
+                  margin-right: 10px;
+                  display: inline;
+                  font-size: 0.8em;
+                }
+                
+                #project div,
+                #company div {
+                  white-space: nowrap;        
+                }
+                
+                table {
+                  width: 100%;
+                  border-collapse: collapse;
+                  border-spacing: 0;
+                  margin-bottom: 20px;
+                }
+                
+
+                
+                table th,
+                table td {
+                  text-align: center;
+                }
+                
+                table th {
+                  padding: 10px 20px;
+                  color: #5D6975;
+                  border-bottom: 1px solid #5D6975;
+                  border-top: 1px solid #5D6975;
+                  white-space: nowrap;        
+                  font-weight: bold;
+                  background-color: #F5F5F5
+                }
+                
+                table .service,
+                table .desc {
+                  text-align: left;
+                }
+                
+                table td {
+                  padding: 20px;
+                  text-align: right;
+                }
+                
+                table td.service,
+                table td.desc {
+                  vertical-align: top;
+                }
+                
+                table td.unit,
+                table td.qty,
+                table td.total {
+                  font-size: 1.2em;
+                }
+                
+                table td.grand {
+                  border-top: 1px solid #C1CED9;
+                  font-weight: 500;
+                }
+                
+                #notices .notice {
+                  color: #5D6975;
+                  font-size: 1.2em;
+                }
+                
+                footer {
+                  color: #5D6975;
+                  width: 100%;
+                  height: 30px;
+                  position: absolute;
+                  bottom: 0;
+                  border-top: 1px solid #C1CED9;
+                  padding: 8px 0;
+                  text-align: center;
+                }
+              </style>
+            </head>
+            <body>
+              <header class="clearfix">
+                <h1 style="">INVOICE</h1>
+                <h4 style="margin-bottom: 0cm;">INV/${id_transaksi}</h4>
+                <br/>
+                <div id="company" class="clearfix">
+                  <h5 style="margin-bottom: 5px">UNTUK</h5>
+                  <div style="margin-bottom: 5px"><span>Penyewa:</span>${nama_pelanggan}</div>
+                  <div><span>Tanggal Transaksi:</span>${tanggal_transaksi}</div>
+                </div>
+                <div id="project">
+                  <h5 style="margin-bottom: 5px">DITERBITKAN ATAS NAMA</h5>
+                  <div><span>Pemilik Kos:</span>${nama_pemilik}</div>
+                </div>
+              </header>
+              <main>
+                <table>
+                  <thead>
+                    <tr>
+                      <th class="service">NAMA KAMAR</th>
+                      <th class="desc">NAMA KOS</th>
+                      <th></th>
+                      <th></th>
+                      <th style="text-align: right;">TOTAL HARGA</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td class="service">${nama_kamar}</td>
+                      <td class="desc">${nama_kos}</td>
+                      <td class="unit"></td>
+                      <td class="qty"></td>
+                      <td class="total">Rp ${jumlah_bayar}</td>
+                    </tr>
+                    <tr>
+                    </tr>
+                    <tr>
+                      <td colspan="4" class="grand total">TOTAL TAGIHAN</td>
+                      <td class="grand total">Rp ${jumlah_bayar}</td>
+                    </tr>
+                  </tbody>
+                </table>
+                <div id="notices">
+                  <h5>Metode Pembayaran :</h5>
+                  <div class="notice">Transfer Bank</div>
+                </div>
+              </main>
+              <footer>
+                Invoice was created on a computer and is valid without the signature and seal.
+              </footer>
+            </body>
+          </html>
         ` }}
       />
     </>
